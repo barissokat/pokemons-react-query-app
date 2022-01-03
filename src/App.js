dfcbf57c9949c446ad7355621597c732b6faeddb
Barış Sokat <barissokat@outlook.com>
2022-01-03 17:26:17 +0300

    Implement React Query

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
+import React from 'react'
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+
+const queryClient = new QueryClient()
+
 function App () {
   return (
-    <div className='App' />
+    <QueryClientProvider client={queryClient}>
+      <Example />
+    </QueryClientProvider>
+  )
+}
+
+function Example () {
+  const { isLoading, error, data } = useQuery('pokemons', () =>
+    fetch('https://pokeapi.co/api/v2/pokemon/6').then(res => res.json())
+  )
+
+  if (isLoading) return 'Loading...'
+
+  if (error) return 'An error has occured: ' + error.message
+
+  return (
+    <div>
+      <h4>{data.name}</h4>
+      <p>weight: {data.weight}</p>
+      <p>height: {data.height}</p>
+    </div>
   )
 }
 
